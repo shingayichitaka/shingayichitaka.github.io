681d3e7d03e8233e817df42babfd9286aedd79cd
shingayichitaka <65043825+shingayichitaka@users.noreply.github.com>
2020-08-19 07:41:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+var x = document.getElementById('mariam');
+
+x.addEventListener('click', function(){
+    window.open("Mariam's Cupcakery/index.html", "_blank");
+});
+
+var b = document.getElementById('tips');
+
+b.addEventListener('click', function(){
+    window.open("Tip Calculator/index.html", "_blank");
+});
+
+var c = document.getElementById('word');
+
+c.addEventListener('click', function(){
+    window.open("Word Length Calculator/index.html", "_blank");
+});
+
+var d = document.getElementById('counter');
+
+d.addEventListener('click', function(){
+    window.open("My Counter/index.html", "_blank");
+});
+
+document.getElementById("facebook"). onclick = function () {
+    location. href = "https://www.facebook.com/profile.php?id=100008763932257";
+    };
+
+    document.getElementById("instagram"). onclick = function () {
+        location. href = "https://www.instagram.com/shingi_sdc/?hl=en";
+        };
+
+        document.getElementById("linkedin"). onclick = function () {
+            location. href = "https://www.linkedin.com/in/shingayi-chitaka-50234b195/";
+            };
+
