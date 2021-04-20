904d50519053450c2c0ccaf38a6b4adb4535d2a1
shingayichitaka <65043825+shingayichitaka@users.noreply.github.com>
2021-04-20 13:00:54 +0200

    Complete redo of portfolio
    Removed old portfolio entirely, and updated it with more responsive projects.

--- My Counter/scripts.js
+++
@@ -1,9 +0,0 @@
-var base = 0;
-document.getElementById("base").innerHTML = base;
-
-function increment() {
-    return document.getElementById("base").innerHTML = base++;
-}
-function decrement() {
-    return document.getElementById("base").innerHTML = base--;
-}
