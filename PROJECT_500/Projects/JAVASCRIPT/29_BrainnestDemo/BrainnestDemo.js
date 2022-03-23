f5de48f40f82b54aecf9c5a2e2ae8ab6f4af800c
shingayichitaka <65043825+shingayichitaka@users.noreply.github.com>
2022-03-23 01:20:00 +0100

    UPDATE

---
+++ PROJECT_500/Projects/JAVASCRIPT/29_BrainnestDemo/BrainnestDemo.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const button = document.querySelector(".click");
+
+button.addEventListener("click", function () {
+  alert(`This is an example of Javascript in action! 
+  I look forward to working with you 😅`);
+});
