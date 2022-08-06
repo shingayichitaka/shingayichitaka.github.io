8817228b3b6a041bf13ece9b42ba3d0f61f45b58
shingayichitaka <shingayidambudzochitaka@gmail.com>
2022-08-06 15:40:26 +0200

    complete overhaul

--- PROJECT_500/Projects/JAVASCRIPT/29_BrainnestDemo/BrainnestDemo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const button = document.querySelector(".click");
-
-button.addEventListener("click", function () {
-  alert(`This is an example of Javascript in action! 
-  I look forward to working with you 😅`);
-});
--- README.md
+++
@@ -1,4 +0,0 @@
-# shingayichitaka.github.io
-Hello,
-This is my portfolio as a Front-End Developer. 
-Feel free to view my code and reach out if you have any questions or would simply like to get in touch.
